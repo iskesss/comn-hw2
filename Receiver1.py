a444277b181b7ce3ffece8cc0773a6302cdcc578
iskesss <jordanlucbouret@gmail.com>
2025-03-05 23:35:58 +0000

    implement part 1

--- Receiver1.py
+++ Receiver1.py
@@ -1,8 +1,38 @@
 # Jordan Bouret, s2795423
-
+import socket
 import sys
+import struct
 
-port = int(sys.argv[1])
-filename = sys.argv[2]
+BYTES_PER_HEADER = struct.calcsize("!B") # ! = big endian, B = uchar 
+BYTES_PER_PACKET = 1024
 
-# TODO: add your implementation here
+def receive_file_over_rdt1(filename, listen_port):
+    listen_ip = "127.0.0.1" # I had to hardcore this for our assignment
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((listen_ip,listen_port))
+    print(f"Receiver listening on {listen_ip}:{listen_port}...")
+
+    with open(filename, "wb") as file:
+        while True:
+            packet, address = sock.recvfrom(BYTES_PER_PACKET)
+            if len(packet) < BYTES_PER_HEADER:
+                continue # we want to ignore incomplete packets
+            flag = struct.unpack("!B", packet[:BYTES_PER_HEADER])[0]
+            data = packet[BYTES_PER_HEADER:] # funky python string parsing magic :)
+            if flag == 1:
+                # We just received an EOF packet, indicating the end of our transmission
+                print("EOF Packet Received! File transfer complete :)") 
+                break
+            file.write(data)
+    sock.close()
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) !=  3:
+        print("Usage: python receiver.py <port> <filename>")
+        sys.exit(1)
+    port = int(sys.argv[1])
+    filename = sys.argv[2] # (save_as)
+    receive_file_over_rdt1(filename, port) 
+
--- Sender1.py
+++ Sender1.py
@@ -1,9 +1,37 @@
-# TODO: replace this comment with your Forename, Surname, and Matriculation Number 
-
+# Jordan Bouret, s2795423
 import sys
+import struct
+import socket
 
-remoteHost = sys.argv[1]
-port = int(sys.argv[2])
-filename = sys.argv[3]
+BYTES_PER_HEADER = struct.calcsize("!B") # ! = big endian, B = uchar
+BYTES_PER_PACKET = 1024
 
-# TODO: add your implementation here
+def send_file_over_rdt1(remoteHost, port, filename):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    with open(filename, "rb") as file: 
+        while True:
+            # Read data such that header + data fits in BYTES_PER_PACKET
+            data = file.read(BYTES_PER_PACKET - BYTES_PER_HEADER)
+            if not data:
+                break
+            header = struct.pack("!B", 0) # sending a flag == 0 tells the receiver there's still data to be transmitted
+            packet = header + data
+            sock.sendto(packet, (remoteHost, port))
+
+    header = struct.pack("!B", 1) # we want to send an EOF packet now that we're out of data to transmit
+     
+    sock.sendto(header,(remoteHost, port)) 
+    sock.close()
+    print("File has successfully been sent")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python sender.py <remoteHost> <port> <filename>")
+        sys.exit(1)  
+    remoteHost = sys.argv[1]
+    port = int(sys.argv[2])
+    filename = sys.argv[3]
+    send_file_over_rdt1(remoteHost,port,filename)
+
+
