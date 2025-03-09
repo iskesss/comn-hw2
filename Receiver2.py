b4e671ffbc9548450799d2229d14085446958aee
iskesss <jordanlucbouret@gmail.com>
2025-03-09 20:29:07 +0000

    folder reshuffling

--- Receiver2.py
+++ Receiver2.py
@@ -1,8 +1,61 @@
-# Jordan Bouret, s2795423 
-
+# Jordan Bouret, s2795423
 import sys
+import struct
+import socket
 
-port = int(sys.argv[1])
-filename = sys.argv[2]
+# Packet size for receiving data
+INCOMING_PACKET_SIZE = 1024
+# Header format: flag (1 byte) and sequence number (1 byte)
+INCOMING_HEADER_FORMAT = "!BB"
+BYTES_PER_HEADER = struct.calcsize(INCOMING_HEADER_FORMAT)
 
-# TODO: add your implementation here
+# ACK format: one unsigned byte for the sequence number
+ACK_FORMAT = "!B"
+BYTES_PER_ACK = struct.calcsize(ACK_FORMAT)
+
+def receive_file_over_rdt3(filename, listen_port):
+    listen_ip = "127.0.0.1" # I had to hardcode this for our assignment
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((listen_ip,listen_port))
+    print("Receiver2 listening on {listen_ip}:{listen_port}...")
+    expected_seq = 0
+    with open(filename, "wb") as outfile:
+        while True:
+            packet, addr = sock.recvfrom(INCOMING_PACKET_SIZE)
+            # Ensure packet is large enough to contain a header
+            if len(packet) < BYTES_PER_HEADER:
+                continue
+            flag, seq = struct.unpack(INCOMING_HEADER_FORMAT, packet[:BYTES_PER_HEADER])
+            if flag == 0:  # current packet holds data
+                if seq == expected_seq:
+                    # Write the payload data to the output file
+                    data = packet[BYTES_PER_HEADER:]
+                    outfile.write(data)
+                    ack = struct.pack(ACK_FORMAT, seq) # acknowledge received packet
+                    sock.sendto(ack, addr)
+                    # Toggle the expected sequence number
+                    expected_seq = 1 - expected_seq
+                else:
+                    # Duplicate packet received: resend the last ACK
+                    last_ack = 1 - expected_seq
+                    ack = struct.pack(ACK_FORMAT, last_ack)
+                    sock.sendto(ack, addr)
+            elif flag == 1:  # EOF packet
+                # Send an ACK for the EOF packet and break the loop
+                ack = struct.pack(ACK_FORMAT, seq)
+                sock.sendto(ack, addr)
+                break
+    sock.close()
+    print("File has been received successfully.")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python3 Receiver1.py <port> <save_received_file_as>")
+        sys.exit(1)
+    listen_port = int(sys.argv[1])
+    filename = sys.argv[2]
+    receive_file_over_rdt3(filename, listen_port)
+
+
+
+
--- Sender2.py
+++ Sender2.py
@@ -1,10 +1,72 @@
-# TODO: replace this comment with your Forename, Surname, and Matriculation Number 
-
+# Jordan Bouret, s2795423
 import sys
+import struct
+import socket
+import time
 
-remoteHost = sys.argv[1]
-port = int(sys.argv[2])
-filename = sys.argv[3]
-retryTimeout = int(sys.argv[4])
+# Configuration parameters
+BYTES_PER_OUTGOING_PACKET = 1024
+# Header: flag (1 byte) and sequence number (1 byte)
+OUTGOING_HEADER_FORMAT = "!BB"
+BYTES_PER_OUTGOING_HEADER = struct.calcsize(OUTGOING_HEADER_FORMAT)
+PAYLOAD_SIZE = BYTES_PER_OUTGOING_PACKET - BYTES_PER_OUTGOING_HEADER
 
-# TODO: add your implementation here
+# ACK header: one unsigned byte for the sequence number
+ACK_FORMAT = "!B"
+BYTES_PER_ACK_HEADER = struct.calcsize(ACK_FORMAT)
+
+def send_file_over_rdt3(remoteHost, port, filename, retry_timeout):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(retry_timeout)
+    seq = 0  # Alternating bit: 0 then 1
+    with open(filename, "rb") as file:
+        while True:
+            data = file.read(PAYLOAD_SIZE)
+            if not data:
+                break
+            # Build a data packet with flag=0 and the current sequence number
+            header = struct.pack(OUTGOING_HEADER_FORMAT, 0, seq)
+            packet = header + data
+            packet_acked = False
+            while not packet_acked:
+                sock.sendto(packet, (remoteHost, port))
+                try:
+                    response, _ = sock.recvfrom(BYTES_PER_ACK_HEADER)
+                    # Unpack the ACK packet to get the acked sequence number
+                    ack_seq, = struct.unpack(ACK_FORMAT, response[:BYTES_PER_ACK_HEADER])
+                    if ack_seq == seq:
+                        packet_acked = True
+                        # Toggle sequence number (alternating bit)
+                        seq = 1 - seq
+                    # If we get an ACK for the wrong sequence, ignore it and wait
+                except socket.timeout:
+                    # Timeout occurred, so we retransmit the packet
+                    continue
+
+    # Send the EOF packet: flag=1 indicates end-of-file.
+    # We use the current sequence number for the EOF packet.
+    eof_header = struct.pack(OUTGOING_HEADER_FORMAT, 1, seq)
+    eof_packet_acked = False
+    while not eof_packet_acked:
+        sock.sendto(eof_header, (remoteHost, port))
+        try:
+            response, _ = sock.recvfrom(BYTES_PER_ACK_HEADER)
+            ack_seq, = struct.unpack(ACK_FORMAT, response[:BYTES_PER_ACK_HEADER])
+            if ack_seq == seq:
+                eof_packet_acked = True
+            # Otherwise, ignore and retransmit EOF packet
+        except socket.timeout:
+            continue
+
+    sock.close()
+    print("File has successfully been sent.")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python3 Sender2.py <ip> <port> <file_to_send> <retry_timeout>")
+        sys.exit(1)
+    remoteHost = sys.argv[1]
+    port = int(sys.argv[2])
+    filename = sys.argv[3]
+    retry_timeout = float(sys.argv[4])
+    send_file_over_rdt3(remoteHost, port, filename, retry_timeout)
