5cd8cb45eb8976ecd904c31483340931f9fd0cf7
iskesss <jordanlucbouret@gmail.com>
2025-03-09 21:03:17 +0000

    backup before switching ubuntu versions

--- Receiver2.py
+++ Receiver2.py
@@ -1,23 +1,21 @@
 # Jordan Bouret, s2795423
+
 import sys
 import struct
 import socket
 
-# Packet size for receiving data
-INCOMING_PACKET_SIZE = 1024
-# Header format: flag (1 byte) and sequence number (1 byte)
-INCOMING_HEADER_FORMAT = "!BB"
+INCOMING_PACKET_SIZE = 1024 # Packet size for receiving data
+INCOMING_HEADER_FORMAT = "!BB" # Header format: flag (1 byte) and sequence number (1 byte)
 BYTES_PER_HEADER = struct.calcsize(INCOMING_HEADER_FORMAT)
 
-# ACK format: one unsigned byte for the sequence number
-ACK_FORMAT = "!B"
+ACK_FORMAT = "!B" # ACK format: one unsigned byte for the sequence number
 BYTES_PER_ACK = struct.calcsize(ACK_FORMAT)
 
 def receive_file_over_rdt3(filename, listen_port):
     listen_ip = "127.0.0.1" # I had to hardcode this for our assignment
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((listen_ip,listen_port))
-    print("Receiver2 listening on {listen_ip}:{listen_port}...")
+    print(f"Receiver2 listening on {listen_ip}:{listen_port}...")
     expected_seq = 0
     with open(filename, "wb") as outfile:
         while True:
@@ -28,20 +26,19 @@ def receive_file_over_rdt3(filename, listen_port):
             flag, seq = struct.unpack(INCOMING_HEADER_FORMAT, packet[:BYTES_PER_HEADER])
             if flag == 0:  # current packet holds data
                 if seq == expected_seq:
-                    # Write the payload data to the output file
+                    # Write the packet's payload data to the output file
                     data = packet[BYTES_PER_HEADER:]
                     outfile.write(data)
                     ack = struct.pack(ACK_FORMAT, seq) # acknowledge received packet
                     sock.sendto(ack, addr)
-                    # Toggle the expected sequence number
-                    expected_seq = 1 - expected_seq
+                    expected_seq = 1 - expected_seq # flip expected seq number 0->1 or 1->0
                 else:
-                    # Duplicate packet received: resend the last ACK
+                    # duplicate packet received: resend the last ACK
                     last_ack = 1 - expected_seq
                     ack = struct.pack(ACK_FORMAT, last_ack)
                     sock.sendto(ack, addr)
             elif flag == 1:  # EOF packet
-                # Send an ACK for the EOF packet and break the loop
+                # acknowledge the EOF packet and break the loop
                 ack = struct.pack(ACK_FORMAT, seq)
                 sock.sendto(ack, addr)
                 break
